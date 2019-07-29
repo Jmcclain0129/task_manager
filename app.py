3ea4703e364c4e685388d58b4256b495e954ddc0
Your Name <you@example.com>
2019-07-29 13:31:42 +0000

    corrected mongo auth issue by updating root pw in mongo cloud app

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:4MongoDB123@myfirstcluster-843eu.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://root:4Root123@myfirstcluster-843eu.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -21,4 +20,3 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
         debug=True)
-
