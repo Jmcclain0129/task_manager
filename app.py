9a57ec580dbf863bbb200f5c88cc54b8b8f8c27f
Your Name <you@example.com>
2019-07-30 12:30:08 +0000

    updated packages added new requirements.txt

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:4Root123@myfirstcluster-843eu.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
@@ -16,7 +15,13 @@ mongo = PyMongo(app)
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
